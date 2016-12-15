b9bcc9bb51ddc068d09702a510dd6e2087834bce
dpmango <xs290@me.com>
2016-12-16 01:21:45 +0300

    update with styles and remove fonts from material

--- src/js/main.js
+++ src/js/main.js
@@ -13,6 +13,13 @@ $(document).ready(function(){
         return false;
 	});
 
+  // Fake dynamics
+  $('.sidebar__nav__list li').on('click', 'li:not(.active)', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    //.closest('div.calculator__form__insurance').find('div.calculator__form__insurance__tabs').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+
   // This will initialize all Material modules
   // var md = new Material();
 
